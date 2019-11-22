f02ffa33ac5b698bc9f10b2a794bfe7d02afa9af
IOINITID <50173108+IOINITID@users.noreply.github.com>
2019-11-22 17:13:49 +0300

    Added styling for block goods

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,13 @@
-var menuButton = document.querySelector('.menu-button');
-var menuButtonShow = document.querySelector('.menu-button__show');
-var menuButtonClose = document.querySelector('.menu-button__close');
-var headerNavigation = document.querySelector('.header__navigation');
+// var menuButton = document.querySelector('.menu-button');
+// var menuButtonShow = document.querySelector('.menu-button__show');
+// var menuButtonClose = document.querySelector('.menu-button__close');
+// var headerNavigation = document.querySelector('.header__navigation');
 
-if (menuButton) {
-  menuButton.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    menuButtonShow.classList.toggle('hide');
-    menuButtonClose.classList.toggle('show');
-    headerNavigation.classList.toggle('show');
-  });
-}
+// if (menuButton) {
+//   menuButton.addEventListener('click', function (evt) {
+//     evt.preventDefault();
+//     menuButtonShow.classList.toggle('hide');
+//     menuButtonClose.classList.toggle('show');
+//     headerNavigation.classList.toggle('show');
+//   });
+// }
